217a14817c0be689d54b85ce319d4f9dbed8da35
Reginaldo <reginaldolribeiro@gmail.com>
2019-03-02 18:33:29 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import GlobalStyle from './styles/global';
+
+import Main from './pages/Main';
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <div className="App" />
+  </Fragment>
+);
+
+export default App;
---
+++ src/pages/Main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Main = () => <h1>Helllo</h1>;
+
+export default Main;
